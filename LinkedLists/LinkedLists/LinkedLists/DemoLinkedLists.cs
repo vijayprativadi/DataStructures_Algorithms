ca8a67844e391b6483f6f3304cd51c7b11e0d201
Vijay Prativadi <vijayprativadi@hotmail.com>
2020-07-26 23:51:07 -0400

    Adding Few Functions on Linked List

---
+++ LinkedLists/LinkedLists/LinkedLists/DemoLinkedLists.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedLists
+{
+    public class Node
+    {
+        public Node(int data)
+        {
+            this.data = data;
+            this.next = null;
+        }
+
+        public Node next;
+        public int data;
+    }
+
+    public class DemoLinkedLists
+    {
+        Node head;
+
+        public void AddNodeAtFirst(int data)
+        {
+            if (head == null)
+            {
+                head = new Node(data);
+            }
+            else
+            {
+                Node node = new Node(data);
+                node.next = head;
+                head = node;
+            }
+        }
+
+        public void AddNodeAtLast(int data)
+        {
+            if (head == null)
+            {
+                head = new Node(data);
+            }
+            else
+            {
+                Node lastNode = GetLastNode();
+                Node newNode = new Node(data);
+                lastNode.next = newNode;
+                newNode.next = null;
+            }
+        }
+
+        public void GetAllNodesWithPointers()
+        {
+            if (head == null)
+            {
+                Console.WriteLine("No nodes present");
+            }
+            else
+            {
+                Node node = head;
+                int counter = 1;
+
+                while (node != null)
+                {
+                    Console.WriteLine("Node at position {0}: ", counter);
+                    Console.WriteLine("Node Value: {0}", node.data);
+                    node = node.next;
+                    counter++;
+                }
+            }
+        }
+
+        public void GetSpecificNodeValueByPosition(int position)
+        {
+            if (head == null)
+            {
+                Console.WriteLine("No nodes present");
+            }
+            else
+            {
+                Node node = head;
+                int counter = 1;
+
+                while (counter <= GetLengthofLinkedList())
+                {
+                    if (counter == position)
+                    {
+                        Console.WriteLine("Node at position {0}: ", counter);
+                        Console.WriteLine("Node Value: {0}", node.data);
+                    }
+
+                    node = node.next;
+                    counter++;
+                }
+            }
+        }
+
+        public int GetLengthofLinkedList()
+        {
+
+            if (head == null) { return 0; }
+
+            Node node = head;
+            int counter = 1;
+            while (node.next != null)
+            {
+                node = node.next;
+                counter++;
+            }
+
+            return counter;
+        }
+
+        private Node GetLastNode()
+        {
+            Node temp = head;
+
+            while (temp.next != null)
+            {
+                temp = temp.next;
+            }
+
+            return temp;
+        }
+
+    }
+
+}
--- LinkedLists/LinkedLists/LinkedLists/Program.cs
+++ LinkedLists/LinkedLists/LinkedLists/Program.cs
@@ -10,6 +10,25 @@ namespace LinkedLists
     {
         static void Main(string[] args)
         {
+            DemoLinkedLists demoLinkedList = new DemoLinkedLists();
+            
+            //Adding Head
+            demoLinkedList.AddNodeAtFirst(34);
+            demoLinkedList.AddNodeAtFirst(77);
+
+            //Adding Tail
+            demoLinkedList.AddNodeAtLast(51);
+
+            //Print Specific Node
+            demoLinkedList.GetSpecificNodeValueByPosition(3);
+
+            //Print Linked List
+            demoLinkedList.GetAllNodesWithPointers();
+
+            //Find Length of Linked List
+            Console.WriteLine("Length of the Linked List is {0}", demoLinkedList.GetLengthofLinkedList());
+
+            Console.ReadKey();
 
         }
     }
